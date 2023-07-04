3f476dc1e0cfadd39b791889222fe03ac2298ea0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-05 02:27:43 +0900

    <feat> : LikeEntity 생성 및 N : M 연관관계 매핑

---
+++ src/main/java/com/newsfeed/board/like/entity/LikeEntity.java
@@ -0,0 +1,27 @@
+package com.newsfeed.board.like.entity;
+// 오세창
+import com.newsfeed.board.post.entity.PostEntity;
+import com.newsfeed.board.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "post_like_table")
+public class LikeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity postEntity;
+}
