1194604e289bc976b707387c85be89182b2067ca
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-05 03:16:06 +0900

    <feat> : LikeRepository 생성

---
+++ src/main/java/com/newsfeed/board/like/repository/LikeRepository.java
@@ -0,0 +1,14 @@
+package com.newsfeed.board.like.repository;
+
+import com.newsfeed.board.like.entity.LikeEntity;
+import com.newsfeed.board.post.entity.PostEntity;
+import com.newsfeed.board.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface LikeRepository extends JpaRepository <LikeEntity, Long> {
+    Optional<LikeEntity> findUserAndPost(UserEntity userEntity, PostEntity postEntity);
+
+}
