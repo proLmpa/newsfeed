e1be3c03be652870d73e46a9c61c906f14e6dedf
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-05 14:56:58 +0900

    <feat> : 게시글 좋아요 반환 타입 변경 및 예외처리 추가

--- src/main/java/com/newsfeed/board/like/controller/LikeController.java
+++ src/main/java/com/newsfeed/board/like/controller/LikeController.java
@@ -1,12 +1,14 @@
 package com.newsfeed.board.like.controller;
 // 오세창
 
+import com.newsfeed.board.common.dto.ApiResponseDto;
 import com.newsfeed.board.common.security.UserDetailsImpl;
 import com.newsfeed.board.like.dto.LikeDto;
 import com.newsfeed.board.like.service.LikeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +20,7 @@ public class LikeController {
 
     private final LikeService likeService;
     @PostMapping("/post/{id}/like")
-    public void insert(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
-        likeService.insert(id, userDetails);
+    public ResponseEntity<ApiResponseDto> insert(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+        return likeService.insert(id, userDetails);
     }
 }
--- src/main/java/com/newsfeed/board/like/service/LikeService.java
+++ src/main/java/com/newsfeed/board/like/service/LikeService.java
@@ -1,5 +1,6 @@
 package com.newsfeed.board.like.service;
 
+import com.newsfeed.board.common.dto.ApiResponseDto;
 import com.newsfeed.board.common.security.UserDetailsImpl;
 import com.newsfeed.board.like.dto.LikeDto;
 import com.newsfeed.board.like.entity.LikeEntity;
@@ -10,6 +11,7 @@ import com.newsfeed.board.user.entity.UserEntity;
 import com.newsfeed.board.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +23,7 @@ public class LikeService {
     private final PostRepository postRepository;
 
     @Transactional
-    public void insert(Long id, UserDetailsImpl userDetails) throws Exception {
+    public ResponseEntity<ApiResponseDto> insert(Long id, UserDetailsImpl userDetails) throws Exception {
 
         UserEntity user = userRepository.findById(userDetails.getId())
                 .orElseThrow(() -> new RuntimeException("Could not found member id : " + userDetails.getId()));
@@ -29,16 +31,24 @@ public class LikeService {
         PostEntity post = postRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Could not found member id : " + id));
 
-        // 이미 좋아요 되어있으면 에러 반환
-        if (likeRepository.findByUserEntityAndPostEntity(user, post).isPresent()) {
-            throw new Exception();
+        try {
+            if (likeRepository.findByUserEntityAndPostEntity(user, post).isPresent()) {
+                // 이미 좋아요 되어있으면 에러 반환
+                throw new Exception("\"Like\" already exists.");
+            } else {
+                // Post의 likes가 1증가
+                post.countLike();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.ok().body(new ApiResponseDto(409L, "LIKE_ALREADY_EXISTS"));
         }
-        // Post의 likes가 1증가
-        post.countLike();
 
         LikeEntity like = new LikeEntity(user, post);
 
         likeRepository.save(like);
+
+        return ResponseEntity.ok().body(new ApiResponseDto(200L, "SUCCESS_REGISTER_LIKE_IN_POST"));
 
     }
 
