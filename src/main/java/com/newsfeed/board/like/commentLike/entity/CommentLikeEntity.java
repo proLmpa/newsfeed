1b6a9fc5e7961fb6bbbbab1057c9cfc72df5ea81
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-05 17:23:11 +0900

    <feat> : 댓글 좋아요 entity 작성

--- src/main/java/com/newsfeed/board/like/commentLike/entity/CommentLikeEntity.java
+++ src/main/java/com/newsfeed/board/like/commentLike/entity/CommentLikeEntity.java
@@ -1,4 +1,30 @@
 package com.newsfeed.board.like.commentLike.entity;
 
+import com.newsfeed.board.comment.entity.CommentEntity;
+import com.newsfeed.board.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "comment_like_table")
 public class CommentLikeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "comment_id")
+    private CommentEntity comment;
+
+    private CommentLikeEntity (UserEntity user, CommentEntity comment) {
+        this.user = user;
+        this.comment = comment;
+    }
 }
