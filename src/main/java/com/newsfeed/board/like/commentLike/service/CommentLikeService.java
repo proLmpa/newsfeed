477a83b14d55a950693c5ab9c33a93fbd4b25ad5
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-06 09:34:40 +0900

    <feat> : 주석 정리

--- src/main/java/com/newsfeed/board/like/commentLike/service/CommentLikeService.java
+++ src/main/java/com/newsfeed/board/like/commentLike/service/CommentLikeService.java
@@ -25,31 +25,27 @@ public class CommentLikeService {
     @Transactional
     public ResponseEntity<ApiResponseDto> insert(Long id, UserDetailsImpl userDetails) throws Exception {
 
-        UserEntity user = userRepository.findById(userDetails.getId())
-                .orElseThrow(() -> new RuntimeException("Could not found member id : " + userDetails.getId()));
-
-        CommentEntity comment = commentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Could not found member id : " + id));
+        // 유저, 댓글 존재하는지 확인
+        UserEntity user = findUserById(userDetails);
+        CommentEntity comment = findCommentById(id);
 
         try {
-            if (commentLikeRepository.findByUserEntityAndCommentEntity(user, comment).isPresent()) {
-                // 이미 좋아요 되어있으면 에러 반환
+            if (commentLikeRepository.findByUserEntityAndCommentEntity(user, comment).isPresent()) {  // 이미 좋아요 되어있으면 에러 반환
                 throw new Exception("\"Like\" already exists.");
-            } else if (comment.getUser().getId().equals(userDetails.getId())) {
+            } else if (comment.getUser().getId().equals(userDetails.getId())) { // 댓글 작성자의 id 와 현재 인가된 유저의 id 를 비교하요 같으면 에러 반환
                 throw new Exception("Same person cannot \"Like\"");
             }
             else {
-                // Post의 likes가 1증가
-                comment.countLike();
+                comment.countLike(); // comment의 likes가 1 증가
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().body(new ApiResponseDto(400L, "LIKE_ERROR"));
         }
 
-        CommentLikeEntity like = new CommentLikeEntity(user, comment);
+        CommentLikeEntity like = new CommentLikeEntity(user, comment); // 어떤 유저와 댓글에 좋아요 달았는지 관계 설정
 
-        commentLikeRepository.save(like);
+        commentLikeRepository.save(like); // DB 저장
 
         return ResponseEntity.ok().body(new ApiResponseDto(200L, "SUCCESS_REGISTER_LIKE_IN_COMMENT"));
 
@@ -58,11 +54,8 @@ public class CommentLikeService {
     @Transactional
     public ResponseEntity<ApiResponseDto> delete(Long id, UserDetailsImpl userDetails) throws Exception {
 
-        UserEntity user = userRepository.findById(userDetails.getId())
-                .orElseThrow(() -> new RuntimeException("Could not found member id : " + userDetails.getId()));
-
-        CommentEntity comment = commentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Could not found member id : " + id));
+        UserEntity user = findUserById(userDetails);
+        CommentEntity comment = findCommentById(id);
 
         try {
             // 좋아요가 존재하는지 확인 후 삭제, 존재하지 않으면 에러 반환
@@ -77,5 +70,15 @@ public class CommentLikeService {
         }
 
         return ResponseEntity.ok().body(new ApiResponseDto(200L, "SUCCESS_DELETE_LIKE_IN_COMMENT"));
+    }
+
+    public UserEntity findUserById(UserDetailsImpl userDetails) {
+        return userRepository.findById(userDetails.getId())
+                .orElseThrow(() -> new RuntimeException("Could not found member id : " + userDetails.getId()));
+    }
+
+    public CommentEntity findCommentById(Long id) {
+        return commentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Could not found member id : " + id));
     }
 }
--- src/main/java/com/newsfeed/board/like/postLike/service/PostLikeService.java
+++ src/main/java/com/newsfeed/board/like/postLike/service/PostLikeService.java
@@ -30,23 +30,21 @@ public class PostLikeService {
         // 본인 게시글에는 좋아요 불가
 
         try {
-            if (postLikeRepository.findByUserEntityAndPostEntity(user, post).isPresent()) {
-                // 이미 좋아요 되어있으면 에러 반환
+            if (postLikeRepository.findByUserEntityAndPostEntity(user, post).isPresent()) { // 이미 좋아요 되어있으면 에러 반환
                 throw new Exception("\"Like\" already exists.");
-            } else if (post.getUser().getId().equals(userDetails.getId())) {
+            } else if (post.getUser().getId().equals(userDetails.getId())) { // 댓글 작성자의 id 와 현재 인가된 유저의 id 를 비교하요 같으면 에러 반환
                 throw new Exception("Same person cannot \"Like\"");
             } else {
-                post.countLike();
+                post.countLike(); // post의 likes가 1 증
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return ResponseEntity.badRequest().body(new ApiResponseDto(400L, "LIKE_ALREADY_EXISTS"));
+            return ResponseEntity.badRequest().body(new ApiResponseDto(400L, "LIKE_ERROR"));
         }
 
-        PostLikeEntity like = new PostLikeEntity(user, post);
+        PostLikeEntity like = new PostLikeEntity(user, post); // 어떤 유저와 게시글에 좋아요 달았는지 관계 설정
 
-        postLikeRepository.save(like);
-
+        postLikeRepository.save(like); // DB 저장
         return ResponseEntity.ok().body(new ApiResponseDto(200L, "SUCCESS_REGISTER_LIKE_IN_POST"));
 
     }
